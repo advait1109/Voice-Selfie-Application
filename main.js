8664c361112312489679984dbcfe6ad6038f645b
advait1109 <86603536+advait1109@users.noreply.github.com>
2022-02-22 16:34:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,20 @@ function start(){
 Recognition.onresult= function run(event){
     var content=event.results[0][0].transcript;
     document.getElementById("textBox").innerHTML=content;
+    speak();
+    Webcam.attach(camera);
 }
+function speak(){
+    var synth=window.speechSynthesis;
+    speakData=document.getElementById("textBox").value;
+    var utterThis=new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
