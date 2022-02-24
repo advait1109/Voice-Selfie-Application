9d1ff022c1a2836bf46dbe6fd1582f887005caa2
advait1109 <86603536+advait1109@users.noreply.github.com>
2022-02-24 16:53:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,15 +8,22 @@ function start(){
 Recognition.onresult= function run(event){
     var content=event.results[0][0].transcript;
     document.getElementById("textBox").innerHTML=content;
-    speak();
+    if (content=="Take my selfie."){
+        speak();
+    }
     Webcam.attach(camera);
+    
 }
 function speak(){
     var synth=window.speechSynthesis;
-    speakData=document.getElementById("textBox").value;
+    speakData="Taking your selfie in 5 seconds";
     var utterThis=new SpeechSynthesisUtterance(speakData);
 
     synth.speak(utterThis);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
 Webcam.set({
     width:360,
@@ -25,3 +32,16 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='selfie_image' src='"+data_uri+"'>";
+        
+    }
+    );
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
